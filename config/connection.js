174a735f6f49bb067d7d1bc30807b0db1293c90f
Alex H <08ahayden@gmail.com>
2018-03-14 20:40:39 -0400

    JAWSDB integration

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,21 @@
 var mysql = require("mysql");
 var dotenv = require("dotenv").config();
 var password = process.env.mysql_password;
+var connection;
 
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: password,
-  database: "burgers_db"
-});
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: password,
+    database: "burgers_db"
+  })
+}
+
 
 connection.connect(function(err){
   if (err){
